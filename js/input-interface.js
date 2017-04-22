d1b2aac30662d63dec1ed70c2f87e00bacdf9e25
Xi Xia <triplextension@gmail.com>
2017-04-22 14:22:48 -0700

    made progress on geolocation promise. Lat Lng display after geolocation is finished, search button appears

--- js/input-interface.js
+++ js/input-interface.js
@@ -7,26 +7,33 @@ var userGeoLocation;
 function userLocation() {
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        userGeoLocation = pos;
-      } , function() {
-        handleLocationError(true, infoWindow, map.getCenter());
+        return new Promise (function(resolve,reject){
+          userGeoLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          resolve(userGeoLocation);
+          reject("didn't work");
+        }).then(function(result){
+          afterPositionFound();
+        }).catch(function(reject){
+          $("#locationMessage").text("Sorry, we couldn't find you.");
+          $("button").fadeIn();
+        })
       });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
   }
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  }
+}
 
+function afterPositionFound (){
+ $("#locatingMessage").text("You are at lat: "+userGeoLocation.lat+" & long: "+userGeoLocation.lng);
+ $("button").fadeIn();
+
+}
 
 $(function(){
-  $.getScript('https://maps.googleapis.com/maps/api/js?key=' + mapKey, function(){userLocation();
-  });
+  $.getScript('https://maps.googleapis.com/maps/api/js?key=' + mapKey);
+  userLocation();
+
 
   $("#searchForm").submit(function(event){
     event.preventDefault();
